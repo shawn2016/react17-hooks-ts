96f7dbd48e9f78247cd7a2be263fdfdbb0f94f54
shawn2016 <787990963@qq.com>
2021-10-28 14:08:39 +0800

    ==

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+ * @Author: your name
+ * @Date: 2021-10-28 13:37:30
+ * @LastEditTime: 2021-10-28 14:08:36
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /react17-hooks-ts/src/index.tsx
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
