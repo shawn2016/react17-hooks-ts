7a13b8a213abbeb070b02d67c54338ab4f7aa8da
shawn2016 <787990963@qq.com>
2021-11-05 17:22:25 +0800

    fix2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-10-28 13:37:30
- * @LastEditTime: 2021-11-05 17:18:47
+ * @LastEditTime: 2021-11-05 17:22:09
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /react17-hooks-ts/src/App.tsx
